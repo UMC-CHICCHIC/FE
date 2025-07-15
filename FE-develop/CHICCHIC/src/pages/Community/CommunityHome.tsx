7d34138cd1d298498de82212b8b31e9026c62f8e
seongmin36 <whtjdals3625@gmail.com>
2025-07-16 02:24:25 +0900

    #23 fix: 잘못 올라간 파일 삭제

--- FE-develop/CHICCHIC/src/pages/Community/CommunityHome.tsx
+++
@@ -1,9 +0,0 @@
-const CommunityHome = () => {
-  return (
-    <>
-      <div></div>
-    </>
-  );
-};
-
-export default CommunityHome;
--- FE-develop/CHICCHIC/src/pages/Community/PerfumeDiary/DiaryHome.tsx
+++
@@ -1,9 +0,0 @@
-const DiaryHome = () => {
-  return (
-    <div>
-      <h1>Diary Home</h1>
-    </div>
-  );
-};
-
-export default DiaryHome;

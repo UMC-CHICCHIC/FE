bd1f82d66a58199aae38ee8900f19a445eb3c504
riveryunny <gayunphone@gmail.com>
2025-08-01 00:50:02 +0900

    #44 feat: 로그인, 회원가입 api 연동, 로그아웃 추가,  UI 수정

---
+++ CHICCHIC/src/apis/axiosInstance.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { getAccessToken } from "../utils/authStorage";
+//import axios { type InternalAxiosRequestConfig } from "axios";
+
+/* interface CustomInternalAxiosRequestConfig extends InternalAxiosRequestConfig {
+  _retry?: boolean;
+} */
+
+// 전역 변수로 refresh 요청의 Promise를 저장해서 중복 요청을 방지
+//let refreshPromise: Promise<string> | null = null;
+
+// 모든 요청에 백엔드 주소 붙여줌
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_SERVER_API_URL, // env 파일
+  withCredentials: true,
+});
+
+// 모든 요청 전에 accessToken을 Authorization 헤더에 추가
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = getAccessToken();
+
+    // accessToken이 존재하면 Authorization 헤더에 Bearer 토큰 형식으로 추가
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
--- CHICCHIC/src/pages/Mypage/Profile.tsx
+++ CHICCHIC/src/pages/Mypage/Profile.tsx
@@ -1,11 +1,13 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { useLogout } from "../../utils/useLogout";
 
 export default function MyHome() {
   const location = useLocation();
   const navigate = useNavigate();
   const currentPath = location.pathname;
-  
+  const logout = useLogout();
+
   // 닉네임 상태 관리 (API 연동 준비)
   const [userNickname, setUserNickname] = useState("(닉네임)");
 
@@ -17,11 +19,6 @@ export default function MyHome() {
     navigate('/mypage/privacy');
   };
 
-  const handleLogoutClick = () => {
-    // 로그아웃 로직 (예: 토큰 삭제, 상태 초기화 등)
-    navigate('/');
-  };
-
   const handleScrapsClick = () => {
     navigate('/mypage/scraps');
   };
@@ -31,51 +28,52 @@ export default function MyHome() {
   };
 
   return (
-    <div className="font-[pretendard] flex min-h-[calc(100vh-64px)] bg-transparent text-[#AB3130]">
+    <div className="font-pretendard min-h-[calc(100vh-64px)] bg-transparent text-[#a8342f] flex flex-col sm:flex-row">
       {/* 사이드 탭 */}
-      <div className="w-91 border-r border-[#AB3130] pt-20 flex flex-col i">
-        <ul className="space-y-13 text-2xl text-[#AB3130] w-full">
-          <li>
+      <div className="w-full sm:w-80 border-b sm:border-b-0 sm:border-r border-[#AB3130] pt-5 sm:pt-20 flex flex-row sm:flex-col justify-center items-center sm:items-stretch sm:justify-stretch sm:h-full sm:min-h-[calc(100vh-64px)]">
+        <ul className="flex flex-row sm:flex-col w-full sm:w-auto gap-0 sm:gap-13 text-2xl text-[#AB3130] items-center justify-between sm:items-stretch sm:justify-stretch">
+          <li className="flex-1 sm:flex-none">
             <button 
               onClick={handleProfileClick}
-              className={`block w-full text-center relative pr-5 py-2 cursor-pointer ${
-                currentPath === '/mypage' 
-                  ? 'font-semibold border-r-4 border-[#AB3130] opacity-100' 
+              className={`flex items-center justify-center w-full h-16 text-center cursor-pointer
+                ${currentPath === '/mypage'
+                  ? 'font-semibold border-b-4 sm:border-b-0 sm:border-r-4 border-[#AB3130] opacity-100'
                   : 'font-semibold opacity-50 hover:opacity-75'
-              }`}
+                }`}
             >
               프로필
             </button>
           </li>
-          <li>
+          <li className="flex-1 sm:flex-none">
             <button 
               onClick={handlePrivacyClick}
-              className={`block w-full text-center cursor-pointer ${
-                currentPath === '/mypage/privacy' 
-                  ? 'font-semibold border-r-4 border-[#AB3130] opacity-100 py-2' 
+              className={`flex items-center justify-center w-full h-16 text-center cursor-pointer
+                ${currentPath === '/mypage/privacy'
+                  ? 'font-semibold border-b-4 sm:border-b-0 sm:border-r-4 border-[#AB3130] opacity-100'
                   : 'font-semibold opacity-50 hover:opacity-75'
-              }`}
+                }`}
             >
               개인정보
             </button>
           </li>
         </ul>
       </div>
-
-      <main className="flex-1 flex flex-col items-start justify-start pt-16 px-8 ml-20">
+      
+      {/* 내용 영역 */}
+      <main className="flex-1 flex flex-col items-center justify-start pt-16 px-8 sm:items-start sm:ml-20">
         <div className="w-55 h-55 rounded-full bg-gray-300 flex items-center justify-center mb-6 mt-20">
         </div>
 
         <div className="text-3xl mt-5 font-semibold mb-2">안녕하세요, {userNickname} 님!</div>
         <button 
-          onClick={handleLogoutClick}
+          onClick={logout}
           className="text-base mt-2 underline text-[#AB3130] cursor-pointer bg-transparent border-none"
         >
           로그아웃
         </button>
 
         <div className="text-xl font-semibold mt-18">페이지 바로가기</div>
-        <div className="flex flex-col lg:flex-row gap-6 ml:gap-35 mt-10 mb-45 text-lg font-light">
+        <div className="flex flex-col lg:flex-row gap-6 xl:gap-38 mt-10 mb-45 text-lg font-light">
           <button
             onClick={handleScrapsClick}
             className=" bg-[#AB3130] text-[#FFFFFF] px-18 py-2 rounded-full cursor-pointer hover:bg-[#8b2a25] transition-colors"

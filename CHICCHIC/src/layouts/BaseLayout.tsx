73195bf2b18df2203dad3c19f74514e7cc97d5d5
riveryunny <gayunphone@gmail.com>
2025-08-05 23:12:30 +0900

    feat: BaseLayout 및 PublicRoute 도입

--- CHICCHIC/src/layouts/BaseLayout.tsx
+++ CHICCHIC/src/layouts/BaseLayout.tsx
@@ -1,13 +1,23 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, Navigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import CommunityTabBar from "../components/Community/CommunityTabBar";
 import ScrollTop from "../components/ScrollTop";
+import { useAuth } from "../hooks/useAuth";
 
-const PublicLayout = () => {
+interface BaseLayoutProps {
+  protectedRoute?: boolean;
+}
+
+const BaseLayout = ({ protectedRoute = false }: BaseLayoutProps) => {
+  const { isLoggedIn } = useAuth();
   const location = useLocation();
-  // community page's navbar
   const isCommunityPage = location.pathname.startsWith("/community");
+
+  if (protectedRoute && !isLoggedIn) {
+    // 로그인 안 되어 있으면 로그인 페이지로 리디렉션
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
 
   return (
     <div className="bg-[#F7F4EF] min-h-screen flex flex-col">
@@ -22,4 +32,4 @@ const PublicLayout = () => {
   );
 };
 
-export default PublicLayout;
+export default BaseLayout;
